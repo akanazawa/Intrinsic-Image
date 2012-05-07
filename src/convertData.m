205c928f6fb268ba670458587e0555ae670a0576
Austin Myers <austin@austin-laptop.(none)>
2012-05-06 23:55:49 -0400

    fixed convert

--- src/convertData.m
+++ src/convertData.m
@@ -13,26 +13,36 @@ n_nan = reshape(n_nan(:,1:3), [640 480 3]);
 d_nan = permute(d_nan, [2 1 3]);
 n_nan = permute(n_nan, [2 1 3]);
 
-d = zeros(480, 640, 3);
-n = zeros(480, 640, 3);
-
-d(:,:,1) = inpaint_nans(d_nan(:,:,1),2);
-d(:,:,2) = inpaint_nans(d_nan(:,:,2),2);
-d(:,:,3) = inpaint_nans(d_nan(:,:,3),2);
-
-n(:,:,1) = inpaint_nans(n_nan(:,:,1),2);
-n(:,:,2) = inpaint_nans(n_nan(:,:,2),2);
-n(:,:,3) = inpaint_nans(n_nan(:,:,3),2);
-
+% Unpacking color values from 32-bit int
 c = uint8(zeros(480, 640, 3));
 c(:,:,1) = uint8(bitand(bitshift(c_pack, -16), uint32(255)))';
 c(:,:,2) = uint8(bitand(bitshift(c_pack, -8), uint32(255)))';
 c(:,:,3) = uint8(bitand(c_pack, uint32(255)))';
 
-[m n d] = size(c);
+% Cropping by 50 pixels to be safe
+% since normal and depth data at boundaries is missing
 c = c(51:end-50, 51:end-50, :);
-n = n(51:end-50, 51:end-50, :);
-d = d(51:end-50, 51:end-50, :);
+n_nan = n_nan(51:end-50, 51:end-50, :);
+d_nan = d_nan(51:end-50, 51:end-50, :);
+
+d = zeros(size(d_nan));
+n = zeros(size(n_nan));
+
+% Inpainting each channel of the depth
+d(:,:,1) = inpaint_nans(d_nan(:,:,1),2);
+d(:,:,2) = inpaint_nans(d_nan(:,:,2),2);
+d(:,:,3) = inpaint_nans(d_nan(:,:,3),2);
+
+% Inpainting each channel of the normals
+n(:,:,1) = inpaint_nans(n_nan(:,:,1),2);
+n(:,:,2) = inpaint_nans(n_nan(:,:,2),2);
+n(:,:,3) = inpaint_nans(n_nan(:,:,3),2);
+
+% Norm of the normals should be 1
+n_norm = sqrt(sum(n.^2,3));
+n(:,:,1) = n(:,:,1)./n_norm;
+n(:,:,2) = n(:,:,2)./n_norm;
+n(:,:,3) = n(:,:,3)./n_norm;
 
 imwrite(c, fullfile(DIR, 'img.png'), 'png');
 save(fullfile(DIR, 'normal.mat'), 'n');
